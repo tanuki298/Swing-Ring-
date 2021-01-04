89d9f4e386b3cd1280f1bf825c56a3f28d815311
tanuki298 <peru2.pso2@gmail.com>
2021-01-05 04:00:04 +0900

    ノーツ判定

--- Assets/Script/Judge.cs
+++ Assets/Script/Judge.cs
@@ -21,20 +21,14 @@ public class Judge : MonoBehaviour
     public int Good;
     public int miss;
 
-    //ノーツの発生時間と判定の間隔を足したもの
-    private int Parfect2;
-    private int Great2;
-    private int Good2;
-    private int miss2;
-
-    private int Parfect3;
-    private int Great3;
-    private int Good3;
-    private int miss3;
-
+    public float StartScale;
+    public float EndScale;
+    public float rate;
+    private int MoveTime;
+    private Vector3 Start;
+    private Vector3 End;
 
     // Start is called before the first frame update
-
     private void Awake()
     {
         //Array.Resize(ref note, Getnote.mu.No.Length);
@@ -46,62 +40,31 @@ public class Judge : MonoBehaviour
 
         Move.gameObject.transform.localScale *= en_size;
 
-    }
-
-    void Start()
-    {
-
+        //円の動きに必要な物
+        MoveTime = Getnote.Out_Timing;
+        StartScale = en_size;
+        Start = new Vector3(StartScale, StartScale, StartScale);
+        End = new Vector3(EndScale, EndScale, EndScale);
     }
 
     // Update is called once per frame
     void Update()
     {
-
         ms2 += Time.fixedDeltaTime;
         ms = (int)(ms2 * 1000);
         s = Getnote.ko;
         //Debug.Log(ms);
-
-        Parfect2 = Getnote.mu.ms[s] + Parfect;
-        Great2 = Getnote.mu.ms[s] + Great;
-        Good2 = Getnote.mu.ms[s] + Good;
-        miss2 = Getnote.mu.ms[s] + miss;
-
-        Parfect3 = Getnote.mu.ms[s] - Parfect;
-        Great3 = Getnote.mu.ms[s] - Great;
-        Good3 = Getnote.mu.ms[s] - Good;
-        miss3 = Getnote.mu.ms[s] - miss;
-
-
-        //s = Getnote.mu.number;
-        //Getnote.Getms(note);
-        //note_Timer = Getnote.mu.ms[Getnote.mu.No[1]];
-
     }
     void FixedUpdate()
     {
-        //if (Good3 > Getnote.mu.ms[s] || Getnote.mu.ms[s] > Good2)
-        //{
-        //    Debug.Log("Parfect " + ms);
-        //}
-        //else if (Great3 > Getnote.mu.ms[s] || Getnote.mu.ms[s] > Great2)
-        //{
-        //    Debug.Log("Great " + ms);
-        //}
-        //else if (Parfect3 < Getnote.mu.ms[s] || Getnote.mu.ms[s] < Parfect2)
-        //{
-        //    Debug.Log("Good " + ms);
-        //}else
-        //{
-        //    Debug.Log("obre " + ms);
-        //}
-
-        Move.transform.localScale -= new Vector3(MoveSpeed, MoveSpeed, MoveSpeed) ;
+        //ノーツの動き
+        rate = (float)ms / MoveTime;
+        Move.transform.localScale = Vector3.Lerp(Start, End, rate);
 
         if (ms > Getnote.Out_Timing * 2)
         {
             s++;
-            Debug.Log("miss ");
+            Debug.Log("miss late");
             ms = 0;
             ms2 = 0;
             gameObject.SetActive(false);
@@ -109,26 +72,51 @@ public class Judge : MonoBehaviour
     }
     void OnTriggerEnter2D(Collider2D collider)
     {
-        //if (collider.gameObject.tag == "Player_L" || collider.gameObject.tag == "Player_R")
-        //{
-            if (Parfect3 < Getnote.mu.ms[s] || Getnote.mu.ms[s] < Parfect2)
+        if (collider.gameObject.tag == "Player_L" || collider.gameObject.tag == "Player_R")
+        {
+            if(ms < miss)
             {
-                Debug.Log("Parfect");
+                Debug.Log("miss! Fast");
                 s++;
                 gameObject.SetActive(false);
             }
-            else if (Great3 < Getnote.mu.ms[s] || Getnote.mu.ms[s] < Great2)
+            else if (ms < miss + Good)
             {
-                Debug.Log("Great");
+                Debug.Log("Good! fast");
                 s++;
                 gameObject.SetActive(false);
             }
-            else if (Good3 < Getnote.mu.ms[s] || Getnote.mu.ms[s] < Good2)
+            else if (ms < miss + Good + Great)
             {
-                Debug.Log("Good");
+                Debug.Log("Great! fast");
                 s++;
                 gameObject.SetActive(false);
             }
-        //}
+            else if (ms < miss + Good + Great + Parfect)
+            {
+                Debug.Log("parfect! fast");
+                s++;
+                gameObject.SetActive(false);
+            }
+            //-------------折り返し----------------------
+            else if (ms < miss + Good + Great + Parfect * 2)
+            {
+                Debug.Log("parfect! late");
+                s++;
+                gameObject.SetActive(false);
+            }
+            else if (ms < miss + Good + Great * 2 + Parfect * 2)
+            {
+                Debug.Log("Great! late");
+                s++;
+                gameObject.SetActive(false);
+            }
+            else if (ms < miss + Good * 2 + Great * 2 + Parfect * 2)
+            {
+                Debug.Log("Good! late");
+                s++;
+                gameObject.SetActive(false);
+            }
+        }
     }
 }
